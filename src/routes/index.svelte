5a1a7fc6af6dd771d0d617c8472bab1955023124
TzeKen <tantzeken05@gmail.com>
2021-12-19 02:43:37 +0000

    commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script>
+    let location ="";
+    let temperature ="";
+    let location ="";
+</script>
+<h1>Weather App</h1>
+
+<form on:submit|preventDefault={getWeather}>
+    <label for="location">Location:</label>
+    <input id="location" bind:value={location} type="text" />
+    <button type="submit">Check</button>
+</form>
